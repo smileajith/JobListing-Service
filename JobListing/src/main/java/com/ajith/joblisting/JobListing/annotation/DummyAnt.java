627536fbb09b5bad67b592725e149c617f038eb2
smileajith <smileajith5@gmail.com>
2025-01-02 02:02:44 +0530

    Job listing service updated.

---
+++ JobListing/src/main/java/com/ajith/joblisting/JobListing/annotation/DummyAnt.java
@@ -0,0 +1,14 @@
+package com.ajith.joblisting.JobListing.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE_USE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DummyAnt {
+
+
+}
